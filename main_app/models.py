dea3733070f4149a6323b13f7fb7feb0461a6f00
Shazammon <ammonorgill@gmail.com>
2022-10-25 17:53:47 -0600

    set up teachers and students tables

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
+
+class Students(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
+
+
+class Teachers(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=50)
+    bio = models.CharField(max_length=1000)
+    average_rating = models.IntegerField(
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(1)
+        ]
+    )
+    years_experience = models.IntegerField()
+    accepting_students = models.BooleanField(
+        default = True
+    )
+
+    def __str__(self):
+        return self.name
