5a82a9567596938cd238ff0fdc2eb6c9e4c7edd5
Shazammon <ammonorgill@gmail.com>
2022-10-26 10:22:44 -0600

    registered tables and set up some dummy data

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+# import all created models from .models
+from .models import Instrument, Student, Teacher, Review, Inquiry
 
 # Register your models here.
+
+admin.site.register(Instrument)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(Review)
+admin.site.register(Inquiry)
