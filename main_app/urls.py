9d3a90023b4e53f15acd2539aaffffab3ea8dbb2
Shazammon <ammonorgill@gmail.com>
2022-10-26 11:01:27 -0600

    partially set up url patterns

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('instruments/', views.instruments, name='instruments'),
+    path('students/', views.students, name='students'),
+    path('teachers/', views.teachers, name='teachers'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('inquiries/', views.inquiries, name='inquiries'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Instrument, Student, Teacher, Review, Inquiry
 
 
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>THIS IS MUSICALY<h1>')
+
+def instruments(request):
+    instruments = Instrument.objects.all()
+    return render(request, 'instruments_list.html', {'instruments': students})
+
+def students(request):
+    students = Student.objects.all()
+    return render(request, 'students_list.html', {'students': students})
+
+def teachers(request):
+    teachers = Teacher.objects.all()
+    return render(request, 'teachers_list.html', {'teachers': teachers})
+
+def reviews(request):
+    reviews = Review.objects.all()
+    return render(request, 'reviews_list.html', {'reviews': reviews})
+
+def inquiries(request):
+    inquiries = Inquiry.objects.all()
+    return render(request, 'inquiries_list.html', {'inquiries': inquiries})
