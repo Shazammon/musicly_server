c5108e2309c1b30483af66bff21b09a394fb38b6
Shazammon <ammonorgill@gmail.com>
2022-10-25 19:10:26 -0600

    set up remaining database tables

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,16 @@
+from email.policy import default
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
-class Students(models.Model):
+class Instrument(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
     name = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
     email = models.CharField(max_length=50)
@@ -13,7 +20,7 @@ class Students(models.Model):
         return self.username
 
 
-class Teachers(models.Model):
+class Teacher(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=50)
@@ -28,11 +35,12 @@ class Teachers(models.Model):
     accepting_students = models.BooleanField(
         default = True
     )
+    instruments = models.ManyToManyField(Instrument)
 
     def __str__(self):
         return self.name
 
-class Reviews(models.Model):
+class Review(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=1500)
     rating = models.IntegerField(
@@ -41,9 +49,29 @@ class Reviews(models.Model):
             MinValueValidator(1)
         ]
     )
-    author = models.ForeignKey(Students,default=None)
-    teacher = models.ForeignKey(Teachers,default=None)
+    author = models.ForeignKey(Student,default=None)
+    teacher = models.ForeignKey(Teacher,default=None)
 
     def __str__(self):
         return self.title
+
+class Inquiry(models.Model):
+    student_name = models.CharField(max_length=50)
+    instrument = models.CharField(max_length=50)
+    content = models.CharField(max_length=1500)
+    availability = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    email = models.CharField(max_length=100)
+    viewed = models.BooleanField(default=False)
+    inquirer = models.ForeignKey(Student,default=None)
+    preferred_teacher = models.ForeignKey(Teacher,default=None)
+
+    def __str__(self):
+        return self.conent
+    
+
+
+
+
+
     
